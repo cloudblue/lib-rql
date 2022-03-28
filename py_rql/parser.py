a7f7d921ab83369dd5759a5e8be4c6b72e8a5955
Maxim Kolyubyakin <maxim.kolyubyakin@ingrammicro.com>
2022-03-28 15:01:12 +0200

    LITE-22733 Added lock for cache update in parsing

--- py_rql/parser.py
+++ py_rql/parser.py
@@ -2,6 +2,8 @@
 #  Copyright © 2022 Ingram Micro Inc. All rights reserved.
 #
 
+from threading import Lock
+
 from cachetools import LFUCache
 from lark import Lark
 from lark.exceptions import LarkError
@@ -15,17 +17,23 @@ class RQLLarkParser(Lark):
         super(RQLLarkParser, self).__init__(*args, **kwargs)
 
         self._cache = LFUCache(maxsize=1000)
+        self._lock = Lock()
 
     def parse_query(self, query):
         cache_key = hash(query)
-        if cache_key in self._cache:
-            return self._cache[cache_key]
+
         try:
-            rql_ast = self.parse(query)
-            self._cache[cache_key] = rql_ast
-            return rql_ast
-        except LarkError:
-            raise RQLFilterParsingError()
+            return self._cache[cache_key]
+        except KeyError:
+
+            try:
+                rql_ast = self.parse(query)
+                with self._lock:
+                    self._cache[cache_key] = rql_ast
+
+                return rql_ast
+            except LarkError:
+                raise RQLFilterParsingError()
 
 
 RQLParser = RQLLarkParser(RQL_GRAMMAR, parser='lalr', start='start')
